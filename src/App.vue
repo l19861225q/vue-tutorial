447a6cee2fd9eb9f97581ea651d3647efad8a33a
l19861225q <l19861225q@gmail.com>
2016-04-27 15:40:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="wrapper">
+    <!-- .navbar -->
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <a href="#" class="navbar-brand">
+          <i class="glyphicon glyphicon-time"></i> 计划表
+        </a>
+        <ul class="nav navbar-nav">
+          <li>
+            <a href="javascript:;" v-link="'/home'">首页</a>
+          </li>
+          <li>
+            <a href="javascript:;" v-link="'/time-entries'">计划列表</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- /.navbar -->
+    <div class="container">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-9">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
